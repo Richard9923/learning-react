8011097d442f145fcb503c01134a9f295c1dbc90
Richard <programmingislife23@gmail.com>
2024-07-12 15:18:14 -0300

    entendendo quao react pode ajudar com interatividade

--- react-app/src/message.tsx
+++ react-app/src/message.tsx
@@ -1,7 +1,10 @@
 
 function Message() {
     // JSX: JavaScript 
-    return <h1>Hello World</h1>;
+    const name = 'Mosh'
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
 };
 
 export default Message;
