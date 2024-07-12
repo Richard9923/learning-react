eae97354113a7da61a6130f6f9f23bf25efc2906
Richard <programmingislife23@gmail.com>
2024-07-12 16:44:03 -0300

    alert component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,13 @@
+import Alert from "./components/alert";
 import ListGroup from "./components/ListGroup";
-import { MouseEvent } from "react";
-function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+function App() {
   return (
-    <>
-      <h1>LIST</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
   );
 }
 
